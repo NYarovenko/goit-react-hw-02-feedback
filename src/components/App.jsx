c3414d07e8313c9b7533d29a8681896d64e4ed34
NYarovenko <NYarovenko.Full.Stack@gmail.com>
2023-08-13 18:00:30 +0300

    block statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Section } from './section/Section';
+import { Statistics } from './statistics/Statistics';
+import style from './App.module.css';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    return Math.round((this.state.good / this.countTotalFeedback) * 100);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div className={style['container--centering']}>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback}
+            positivePercentage={this.countPositiveFeedbackPercentage}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,19 @@
+import styles from './Statistics.module.css';
+
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <ul className={styles.container_statistics}>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+      <li>Total: {total}</li>
+      <li>Positive feedback: {positivePercentage}%</li>
+    </ul>
+  );
+};
